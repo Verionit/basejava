ec61d4693b68cb4eb5ab691e823471728d6e0f6a
Kurilenko Aleksandr <me@nobito.ru>
2024-11-04 16:36:06 +0300

    save метод исправлен

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,9 +3,15 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
+        if(size == 0){
+            System.out.println("Список чист!");
+        } else {
+            for (int i = 0; i < size; i++) {
+                storage[i] = null;
+            }
+            System.out.println("Список чист!");
         }
+        size = 0;
     }
 
     void save(Resume r) {
@@ -25,7 +31,13 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i <= size; i++) {
+
+        if (size == 0) {
+            System.out.println("Список пуст!");
+            return null;
+        }
+
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -41,10 +53,8 @@ public class ArrayStorage {
             if (storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
 
-
                 Resume[] temp = new Resume[size - i];
                 System.arraycopy(storage, i, temp, 0, size - i);
-
 
                 size--;
                 break;
