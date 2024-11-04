35e52056e62a8e225cb3d02050793ef6d6a3049b
Kurilenko Aleksandr <me@nobito.ru>
2024-11-04 19:25:09 +0300

    исправлен метод getAll, упрощен метод delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,41 +1,33 @@
-import java.util.Arrays;
-
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size = 0;
 
     void clear() {
         if (size == 0) {
-            System.out.println("Список чист!");
+            System.out.println("Массив чист!");
         } else {
             for (int i = 0; i < size; i++) {
                 storage[i] = null;
             }
-            System.out.println("Список чист!");
+            System.out.println("Массив чист!");
         }
         size = 0;
     }
 
     void save(Resume resume) {
-        if (size == 0) {
-            storage[size] = resume;
-            size++;
-        } else {
-            for (int i = 0; i < size; i++) {
-                if (resume.uuid.equals(storage[i].uuid)) {
-                    System.out.println("Данный ID уже есть в списке!");
-                    return;
-                }
+        for (int i = 0; i < size; i++) {
+            if (resume.uuid.equals(storage[i].uuid)) {
+                System.out.println("Данный ID уже есть в массиве!");
+                return;
             }
-            storage[size] = resume;
-            size++;
         }
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
-
         if (size == 0) {
-            System.out.println("Список пуст!");
+            System.out.println("Массив пуст!");
             return null;
         }
 
@@ -49,18 +41,10 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int tempPosition;
-
         for (int i = 0; i < size; i++) {
-            assert storage[i] != null;
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                tempPosition = i;
-
-                Resume[] tempArray = new Resume[size - tempPosition - 1];
-                System.arraycopy(storage, i + 1, tempArray, 0, size - i - 1);
-                System.out.println(Arrays.toString(tempArray));
-                System.arraycopy(tempArray, 0, storage, i, size - 1);
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
                 return;
             }
@@ -72,9 +56,9 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] tempArray = new Resume[size];
-        System.arraycopy(storage, 0, tempArray, 0, size);
-        return tempArray;
+        Resume[] allResumes = new Resume[size];
+        System.arraycopy(storage, 0, allResumes, 0, size);
+        return allResumes;
     }
 
     int size() {
